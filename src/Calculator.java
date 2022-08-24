919894785ba130ed2c56388c2c13830cd504c216
Oskar Migowski <o.migowski@protonmail.com>
2022-08-24 10:36:01 +0200

    Visual changes, added few message dialog boxes

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,7 @@ import org.xml.sax.SAXException;
 
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,16 +13,19 @@ public class Calculator extends JFrame {
     private JFormattedTextField calculatedOutput;
     private JComboBox currencyList;
     HashMap<String, Double> currencies = new HashMap<String, Double>();                 //hashmap przechowujacy symbol i kurs
-    DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>();
+    DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>();            //combobox model, używany do aktualizacji danych w liście walut
 
     Calculator() {
+        Color bg = new Color(43, 43, 43);                      //kolor tła aplikacji
         this.setTitle("CurrencyCalculator");                            //nadanie tytułu aplikacji
         this.setSize(500, 200);                            //zmiana rozmiaru okna
+        this.setLocationRelativeTo(null);                              //pojawienie się okna na środku ekranu
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);           //zatrzymanie procesu przy zamknięciu okna
         this.setContentPane(mainPanel);
         ImageIcon icon = new ImageIcon("icon.png");
         this.setIconImage(icon.getImage());                            //zmiana ikony aplikacji
         this.setVisible(true);
+        mainPanel.setBackground(bg);                                    //nadanie koloru tła aplikacji
     }
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
--- src/FileHandler.java
+++ src/FileHandler.java
@@ -12,34 +12,36 @@ public class FileHandler {
 
     FileHandler(Calculator calc) throws ParserConfigurationException, IOException, SAXException {                          //TODO pobieranie pliku z internetu
         String fileName;
-        fileName = JOptionPane.showInputDialog("Podanie nazwy pliku.", "Podaj nazwę pliku z danymi.");
+        fileName = JOptionPane.showInputDialog(null, "Podaj nazwę pliku z danymi.", "Podanie nazwy pliku", JOptionPane.INFORMATION_MESSAGE);        //komunikat proszący o podanie nazwy pliku .xml
         File xml = new File(fileName);
         if(!xml.isFile()){
             int a = JOptionPane.showConfirmDialog(null, "Nie znaleziono pliku. Czy pobrać go z internetu?");
             if(a == JOptionPane.YES_OPTION){
                 //pobranie pliku z internetu
             } else {
-                System.exit(1);                     //jeśli użytkownik nie chce pobierać pliku, wychodzimy z programu
+                JOptionPane.showMessageDialog(null, "Anulowanie pracy programu.", "Błąd pliku", JOptionPane.WARNING_MESSAGE);
+                System.exit(1);                                                                                                                                         //jeśli użytkownik nie chce pobierać pliku, wychodzimy z programu oraz wyświetlamy mu odpowiedni komunikat
             }
         }
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(xml);
-        NodeList nodeList = document.getElementsByTagName("Cube");      //plik xml zawiera Cube w nagłówku
+        NodeList nodeList = document.getElementsByTagName("Cube");                                                                                                           //plik xml zawiera Cube w nagłówku
 
         for(int i = 0; i < nodeList.getLength(); i++){
             Node node = nodeList.item(i);
             if(node.getNodeType() == Node.ELEMENT_NODE){
                 Element element = (Element) node;
                 if(!element.hasAttribute("rate")){
-                    continue;                                                               //zabezpieczenie na wypadek odczytania danych nie dotyczących walut (np.data)
+                    continue;                                                                               //zabezpieczenie na wypadek odczytania danych nie dotyczących walut (np.data)
                 }
-                String stemp = element.getAttribute("currency");                      //tymczasowe zmienne do przechowania danych z xml
+                String stemp = element.getAttribute("currency");                                      //tymczasowe zmienne do przechowania danych z xml
                 Double dtemp = Double.parseDouble(element.getAttribute("rate"));
-                calc.currencies.put(stemp, dtemp);                                               //dodanie danych do hashmapy
-                calc.model.addElement(stemp);                                                    //dodanie danych do modelu celem aktualizacji danych w rozwijanej liście
+                calc.currencies.put(stemp, dtemp);                                                          //dodanie danych do hashmapy
+                calc.model.addElement(stemp);                                                               //dodanie danych do modelu celem aktualizacji danych w rozwijanej liście
             }
         }
+        JOptionPane.showMessageDialog(null, "Poprawno wczytano dane z pliku.", "Informacja", JOptionPane.INFORMATION_MESSAGE);
     }
 }
